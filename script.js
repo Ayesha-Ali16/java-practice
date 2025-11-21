ae5ba4e3ebbd4b8edcac5498dce7f99aa25f4c0f
Ayesha-Ali16 <aisha023456789@gmail.com>
2025-11-21 22:29:36 +0500

    practice  done

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 
 console.log("This is a sample JavaScript file.");
 
-alert("HELOO);
+alert("HELOO");
+let a = 10;
+let b = 20;
+console.log(a + b);
+let c = a/b;
+console.log(c);
+let d = a*b;
+console.log(d);
+let e = b-a;
+let name = "AYESHA";
+let age = 16;
+let city = "karachi";
+console.log("My name is " + name + ", I am " + age + " years old and I live in " + city + ".");
+const fvrtcolor = "black and blue";
+console.log("My fvrt color is "+fvrtcolor) 
