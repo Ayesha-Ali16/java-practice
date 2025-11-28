752dd6f794a9be1f7b127a613e9f98b3df90ef29
Ayesha-Ali16 <aisha023456789@gmail.com>
2025-11-28 22:19:09 +0500

    done

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 
 console.log("This is a sample JavaScript file.");
 
-alert("HELOO");
+ alert("hello world! \nThis is my first alert box!");
+
 let a = 10;
 let b = 20;
 console.log(a + b);
@@ -16,3 +17,16 @@ let city = "karachi";
 console.log("My name is " + name + ", I am " + age + " years old and I live in " + city + ".");
 const fvrtcolor = "black and blue";
 console.log("My fvrt color is "+fvrtcolor) 
+console.log(typeof a)
+let $a = "AYESHA ALI";
+let $b = true;
+let $c = null;
+let $d = undefined;
+let $e = 123.45;
+const pi = 3.14;
+console.log(typeof $a)
+console.log(typeof $b)
+console.log(typeof $c)
+console.log(typeof $d)
+console.log(typeof $e)
+console.log(typeof pi)
